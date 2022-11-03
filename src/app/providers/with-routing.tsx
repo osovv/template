2b214cae0ccb61e0a00e9a96841cbadcc58dae7c
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2022-11-03 10:41:27 +0300

    Remove GH Pages support
    It makes deploying and routing much more complicated than it needs to be. Just use Vercel instead

--- src/app/providers/with-routing.tsx
+++ src/app/providers/with-routing.tsx
@@ -3,12 +3,10 @@ import { RouterProvider } from 'atomic-router-react';
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import { notFoundRoute, routesMap } from '~/pages';
-import { basePath } from '~/shared/routes';
 
 const history = createBrowserHistory();
 
 const router = createHistoryRouter({
-  base: basePath,
   routes: routesMap,
   notFoundRoute,
 });
